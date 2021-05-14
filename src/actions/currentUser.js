6f8ef9e0e1acf5e3320176d5ea4be925dd84d84f
ohletamyleta <ohletamyleta@gmail.com>
2021-05-14 14:07:10 -0400

    debug login route

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -2,11 +2,10 @@ import { clearLoginForm } from "../actions/loginForm";
 import { resetSignupForm } from "../actions/signupForm";
 import store from "../store";
 
-export const setCurrentUser = (user, spells) => {
+export const setCurrentUser = (user) => {
   return {
     type: "SET_CURRENT_USER",
-    user,
-    spells,
+    user
   };
 };
 
