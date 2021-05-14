e02cc51e4c7143c864ffedf9c051358af6984b38
ohletamyleta <ohletamyleta@gmail.com>
2021-05-13 20:00:18 -0400

    build actions & reducers

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -16,3 +16,66 @@ export const clearCurrentUser = () => {
   };
 };
 
+export const login = (credentials) => {
+  return (dispatch) => {
+    return fetch("http://localhost:3001/api/v1/login", {
+      credentials: "include",
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(credentials),
+    })
+      .then((r) => r.json())
+      .then((user) => {
+        if (user.error) {
+          alert(user.error);
+        } else {
+          dispatch(setCurrentUser(user));
+        }
+      });
+  };
+};
+
+export const logout = () => {
+  return (dispatch) => {
+    dispatch(clearCurrentUser());
+    return fetch("http://localhost:3001/api/v1/logout", {
+      credentials: "include",
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+
+      .then(() => {
+        dispatch(clearLoginForm());
+      });
+  };
+};
+
+export const signup = (credentials) => {
+  return (dispatch) => {
+    const userInfo = {
+      user: credentials,
+    };
+    console.log("userInfo", userInfo);
+
+    return fetch("http://localhost:3001/api/v1/signup", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userInfo),
+    })
+      .then((r) => r.json())
+      .then((user) => {
+        if (user.errors) {
+          alert(user.errors);
+        } else {
+          console.log("response data", user);
+
+          dispatch(setCurrentUser(user));
+          dispatch(resetSignupForm());
+        }
+      })
+      .catch(console.log);
+  };
+};
